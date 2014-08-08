dc973c2a0136f52de83861a732be198c12f095bd
wastegas <wastegas@gmail.com>
2014-08-07 22:42:06 -1000

    define account struct

--- include/account.h
+++ include/account.h
@@ -1,12 +1,11 @@
 #ifndef _ACCOUNT_H_
 #define _ACCOUNT_H_
 
-typedef struct
-{
-    char username[25];
-    unsigned char password[SHA_DIGEST_LENGTH];
-    char uuid[40];
-} account;
+typedef struct {
+	char		username  [25];
+	unsigned char	password[SHA_DIGEST_LENGTH];
+	char		uuid      [40];
+}		account;
 
 
 #endif
