a90e352da7ce78bd99e1f74dd98670347979cd03
wastegas <wastegas@gmail.com>
2014-08-11 09:04:17 -1000

    completed core logic

--- src/adduser.c
+++ src/adduser.c
@@ -3,12 +3,22 @@
 #include "adduser.h"
 #include "account.h"
 #include "openfile.h"
+#include "getinput.h"
 
+/* adduser - create a new user and save to file
+ */
 void 
 adduser(struct account*)
 {
   char          username[25];
   unsigned char password[SHA_DIGEST_LENGTH];
   char          uuid[40];  
+  char          input[25];
+  char          stringpassword[25];
+  char          *saltedpass = malloc(strlen(stringpassword) + 36);
 
+  getinput(username, stringpassword, ADD);
+  generateuuid(uuid);
+  saltedpass = concatinate(stringpassword, uuid);
+  generate_sha1_hash(saltedpassword, password);
 }
