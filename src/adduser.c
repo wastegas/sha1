10f4a9c7e735862040d23757f17730705f83571b
wastegas <wastegas@gmail.com>
2014-08-13 08:00:17 -1000

    added writercord function

--- src/adduser.c
+++ src/adduser.c
@@ -5,10 +5,12 @@
 #include "openfile.h"
 #include "getinput.h"
 
+static void writerecord(account*);
+
 /* adduser - create a new user and save to file
  */
 void 
-adduser(struct account*)
+adduser()
 {
   char          username[25];
   unsigned char password[SHA_DIGEST_LENGTH];
@@ -16,9 +18,33 @@ adduser(struct account*)
   char          input[25];
   char          stringpassword[25];
   char          *saltedpass = malloc(strlen(stringpassword) + 36);
+  account *_a   = malloc(sizeof(*_a));
+
 
   getinput(username, stringpassword, ADD);
   generateuuid(uuid);
   saltedpass = concatinate(stringpassword, uuid);
   generate_sha1_hash(saltedpassword, password);
+  
+  strncpy(_a->username, username, 25);
+  strncpy(_a->password, password, SHA_DIGEST_LENGTH);
+  strncpy(_a->uuid,  uuid, 36);
+  _a->uuid[36] = '\0';
+
+  writerecord(_a);
+}
+
+void
+writerecord(account *_a)
+{
+  FILE *fp = openFile("user.dat", APPENDING);
+
+  if ((fwrite(_a, sizeof(account), 1, fp)) < 1 )
+    {
+      fprintf(stderr, "error appending record");
+      free(_a);
+      fclose(fp);
+      exit(EXIT_FAILURE);
+    }
+  fpclose(fp);
 }
